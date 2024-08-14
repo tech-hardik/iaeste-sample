7d49441a5e639b63b71414c10cfcf2a1ced9a113
sharma.hardik2005@gmail.com <sharma.hardik2005@gmail.com>
2024-08-14 22:25:49 +0000

    Edited Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -14,3 +14,12 @@ class Home(HomeTemplate):
     self.repeating_panel_1.items = app_tables.pricing.search()
 
     # Any code you write here will run when the form opens.
+
+  def primary_color_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    
+    pass
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
